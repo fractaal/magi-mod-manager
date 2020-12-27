14cc09bcf40a82fbf5f3e91c370360759b3f5345
Jude <benedict.jude.rocat@gmail.com>
2020-12-28 01:27:20 +0800

    ➕ Create ListItem Component

---
+++ src/components/ListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="list-item" :class="enabled ? '' : 'disabled'">
+    <img :src="image" class="mod-logo">
+    <div style="width: 100%;">
+      <h2>{{title}}</h2>
+      <p>{{content}}</p>
+    </div>
+    <div class="row">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+  props: ['title', 'content', 'image', 'enabled'],
+};
+</script>
+
+<style lang="scss" scoped></style>
