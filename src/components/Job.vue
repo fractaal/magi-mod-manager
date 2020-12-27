d1cef9905f0079bc552a83b21f344ece6c743b9f
Jude <benedict.jude.rocat@gmail.com>
2020-12-27 20:49:17 +0800

    ➕ Primitive functionality

---
+++ src/components/Job.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="job">
+    <div class="upper-job">
+      <h2>{{title}}</h2>
+      <p>{{subtitle}}</p>
+    </div>
+    <div class="middle-job">
+      <div class="progress-bar" :class="styling" :style="`width: ${progress}%`"></div>
+    </div>
+    <div class="lower-job">
+      <p>{{progress}}% - {{status}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Job",
+  data() {
+    return {};
+  },
+  props: ['title', 'subtitle', 'status', 'progress', 'type', 'running', 'complete', 'failed'],
+  computed: {
+    styling: function() {
+      if (this.failed) {
+        return 'error';
+      } else if (this.complete) {
+        return 'complete';
+      } else if (this.running) {
+        return 'running';
+      } else {
+        return '';
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/MagiButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="round-button" @click="$emit('click')">
+    <fai :icon="icon" :size="size"/>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "MagiButton",
+  props: ['icon', 'size'],
+};
+</script>
