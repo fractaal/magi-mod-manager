b0bfdd10cb2b37c1b97a21ca999e6a7e8a611b4b
Jude <benedict.jude.rocat@gmail.com>
2020-12-28 01:25:59 +0800

    ➕ Add icons

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,19 @@ import router from './router'
 
 // Icons
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHome, faSearch, faTimes, faWindowMaximize, faWindowMinimize } from '@fortawesome/free-solid-svg-icons';
+import { faDownload, faHome, faPowerOff, faRedoAlt, faSearch, faTimes, faWindowMaximize, faWindowMinimize } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faHome, faSearch, faWindowMinimize, faWindowMaximize, faTimes)
+library.add(faHome, faSearch, faWindowMinimize, faWindowMaximize, faTimes, faDownload, faRedoAlt, faPowerOff)
 
 // Modals
 import VModal from 'vue-js-modal'
 
-Vue.config.productionTip = false
 Vue.use(VModal)
 
-Vue.component("fai", FontAwesomeIcon);
+Vue.component("fai", FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
