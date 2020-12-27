6ab0183d0fc8568d8f9573fa866b18421cbedda2
Jude <benedict.jude.rocat@gmail.com>
2020-12-28 01:29:30 +0800

    ➕ Add Routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,16 @@ import VueRouter, { RouteConfig } from 'vue-router'
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-  
+  {
+    path: "/home",
+    name: "Home",
+    component: () => import("@/views/Home.vue"),
+  },
+  {
+    path: "/search",
+    name: "Search",
+    component: () => import("@/views/Search.vue"),
+  }
 ]
 
 const router = new VueRouter({
