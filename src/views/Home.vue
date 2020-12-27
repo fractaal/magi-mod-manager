008fe6fab25530ac7d51843ca905c189ddff9932
Jude <benedict.jude.rocat@gmail.com>
2020-12-28 01:29:47 +0800

    ➕ Your Mods Screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <h1>YOUR MODS</h1>
+    <br>
+    <div class="list">
+      <list-item v-for="mod in shared.magi.profileManager.activeProfile.mods"
+      :key="mod.id"
+      :title="mod.name"
+      :image="mod.pictureUrl"
+      :content="mod.summary"
+      :enabled="mod.active"
+      >
+      <magi-button icon="power-off" size="lg" @click="mod.toggle()"/>
+      </list-item>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script>
+import ListItem from '@/components/ListItem.vue';
+import MagiButton from '@/components/MagiButton.vue';
 
-export default Vue.extend({
-  name: 'Home',
-  components: {
-    HelloWorld,
+import shared from '../store';
+
+export default {
+  components: { ListItem, MagiButton },
+  name: "Home",
+  data() {
+    return {
+      shared
+    };
   },
-});
+};
 </script>
