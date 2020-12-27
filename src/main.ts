0877696f601023008fbc51225145d336ecb8d7cd
Jude <benedict.jude.rocat@gmail.com>
2020-12-27 20:48:33 +0800

    ➕ Add icons & modal components

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Icons
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHome, faSearch, faTimes, faWindowMaximize, faWindowMinimize } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faHome, faSearch, faWindowMinimize, faWindowMaximize, faTimes)
+
+// Modals
+import VModal from 'vue-js-modal'
+
 Vue.config.productionTip = false
+Vue.use(VModal)
+
+Vue.component("fai", FontAwesomeIcon);
 
 new Vue({
   router,
